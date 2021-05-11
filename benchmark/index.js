3bd6f958826732a426e3bfdfb05f13dd746ba1bc
designeng <denissavenok@gmail.com>
2021-05-11 14:24:47 +0300

    Wire spec with workers & calc wrapped in Promise.all

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,37 +1,85 @@
 const wire = require('wire');
 const Benchmark = require('benchmark');
 
-const fibonacci = require('../test/math/fibonacci')
-
-const spec = require('../test/spec');
-const specWithPromiseAll = require('./specWithPromiseAll');
+const JestWorkerPlugin = require('..');
+const fibonacci = require('../test/math/fibonacci');
 
 const suite = new Benchmark.Suite;
 
-function runSpecWithWorkers() {
-    return wire(spec);
-}
+const NUM = 20;
 
-function runSpecWithPromiseAll() {
-    return wire(specWithPromiseAll);
-}
+wire({
+    $plugins: [
+        JestWorkerPlugin
+    ],
 
-suite
-// .add('Workers', runSpecWithWorkers)
-// .add('Promise.all', runSpecWithPromiseAll)
+    worker: {
+        createJestWorker: {
+            workerPath: __dirname + '/../test/workers/index.js',
+            workerOptions: {
+                exposedMethods: [
+                    'init',
+                    'calculate'
+                ],
+                numWorkers: 4,
+                maxRetries: 10,
+                forkOptions: {
+                    silent: false
+                }
+            },
+            additionalOptions: {
+                initImmediately: true
+            }
+        }
+    },
 
-.add('Workers', () => {
-    return fibonacci(20);
+    runWorkersFn: {
+        create: {
+            module: (worker) => function runWorkersFn() {
+                return Promise.all([
+                    worker.calculate(NUM),
+                    worker.calculate(NUM),
+                    worker.calculate(NUM),
+                    worker.calculate(NUM),
+                ])
+            },
+            args: [
+                {$ref: 'worker'}
+            ]
+        }
+    },
+
+    runPromiseAllFn: {
+        create: {
+            module: () => function runPromiseAllFn() {
+                return Promise.all([
+                    fibonacci(NUM),
+                    fibonacci(NUM),
+                    fibonacci(NUM),
+                    fibonacci(NUM),
+                ])
+            },
+            args: []
+        }
+    }
+}).then(context => {
+    const {
+        runWorkersFn,
+        runPromiseAllFn,
+    } = context;
+
+    suite
+        .add('Workers', runWorkersFn)
+        .add('Promise.all', runPromiseAllFn)
+
+        .on('cycle', (event) => {
+            console.log(String(event.target));
+        })
+        .on('complete', function() {
+            console.log('Fastest is ' + this.filter('fastest').map('name'));
+
+            context.destroy().then(process.exit);
+        })
+
+        .run({ 'async': true });
 })
-.add('Promise.all', () => {
-    return fibonacci(10);
-})
-
-.on('cycle', (event) => {
-    console.log(String(event.target));
-})
-.on('complete', function() {
-    console.log('Fastest is ' + this.filter('fastest').map('name'));
-})
-
-.run({ 'async': true });
